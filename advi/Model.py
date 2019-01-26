2799daf2ded2218cef71fe0b65c2ac2f630ec393
Arthur Lui <luiarthur@gmail.com>
2019-01-26 10:01:57 -0800

    added docString

--- advi/Model.py
+++ advi/Model.py
@@ -90,15 +90,63 @@ class Model(abc.ABC):
         return ll + lprior - lq
 
     def compute_elbo_mean(self, data, v, nmc, minibatch_info):
+        """
+        Compute the mean of the elbo via Monte Carlo integration
+        
+        The number of MC samples (nmc) can be as little as 1 in practice.
+        But for some models, nmc=2 may be necessary. nmc >= 10 could be
+        overkill for most if not all problems.
+
+        data: data
+        v: variational parameters (real space)
+        nmc: number of MC samples
+        minibatch_info: information about minibatch
+        """
         mini_data = self.subsample_data(data, minibatch_info)
         return torch.stack([self.compute_elbo(v, mini_data, minibatch_info)
                             for i in range(nmc)]).mean()
 
     def msg(self, t, v):
+        """
+        an optional message to print at the end of each iteration.
+
+        t: iteration number
+        v: variational parameters (real space)
+        """
         pass
         
-    def fit(self, data, niters:int=1000, nmc:int=10, lr=1e-3, minibatch_info=None, seed:int=1,
-            eps=1e-8, init=None, print_freq:int=10, verbose:int=1):
+    def fit(self, data, niters:int=1000, nmc:int=2, lr:float=1e-2,
+            minibatch_info=None, seed:int=1, eps:float=1e-6, init=None,
+            print_freq:int=10, verbose:int=1):
+        """
+        fir the model.
+
+        data: data
+        niter: max number of iterations for ADVI
+        nmc: number of MC samples for estimating ELBO mean (default=2). nmc=1
+             is usually sufficient. nmc >= 2 may be required for noisy gradients.
+             nmc >= 10 is overkill in most situations.
+        lr: learning rate (> 0)
+        minibatch_info: information on minibatches
+        seed: random seed for torch (for reproducibility)
+        eps: threshold for determining convergence. If `abs((elbo_curr /
+             elbo_prev) -1) < eps`, then ADVI exits before `niter` iterations.
+        init: initial values for variational parameters (in real space). This has
+              the same for as the output.
+        print_freq: how often to print ELBO value during algorithm. For monitoring
+                    status of ADVI. (default=10, i.e. print every 10 iterations.)
+        verbose: an integer indicating how much output to show. defaults to 1, 
+                 which prints the ELBO. Setting verbose=0 will turn off all outputs.
+
+        returns: dictionary with keys 'v' and 'elbo', where 'v' is the
+                 variational parameters in real space, and 'elbo' is the 
+                 ELBO history.
+        """
+
+        assert(nmc >= 1)
+        assert(lr > 0)
+        assert(eps >= 0)
+
 
         if init is not None:
             v = copy.deepcopy(init)
