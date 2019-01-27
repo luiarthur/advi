69b5f69634656386ab80a3cee0b4ebef46e9d0ce
Arthur Lui <luiarthur@gmail.com>
2019-01-26 22:05:19 -0800

    made version 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='advi',
-      version='0.1',
+      version='0.2',
       description='My personal ADVI library',
       url='http://github.com/advi',
       author='Arthur Lui',
