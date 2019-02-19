e9c6d522d68004d02822800527a7d717624b0d65
Arthur Lui <luiarthur@gmail.com>
2019-02-19 15:18:27 -0800

    stick breaking process works. need to understand it.

--- examples/Gmm_torch_trans.py
+++ examples/Gmm_torch_trans.py
@@ -7,28 +7,24 @@ import matplotlib.pyplot as plt
 
 import advi
 from torch.distributions.transforms import StickBreakingTransform
+from VarParam import VarParam
 
 class Gmm(advi.Model):
     """
     y[i] ~ sum_{k=1}^K Normal(y[i] | mu_k, sig_k)
     """
-    def __init__(self, K:int, priors=None, dtype=torch.float64, device="cpu"):
+    def __init__(self, K:int, N:int, priors=None, dtype=torch.float64, device="cpu"):
         self.dtype = dtype
         self.device = device
         self.priors = priors
         self.K = K
-        self.sbt = StickBreakingTransform(1)
+        self.N = N
+        self.sbt = StickBreakingTransform(0)
 
     def init_vp(self):
-        v = {'mu': None, 'sig': None, 'w': None}
-        for key in v:
-            if key is not 'w':
-                v[key] = torch.randn((self.K, 2), device=self.device, dtype=self.dtype)
-            else:
-                v[key] = torch.randn((self.K - 1, 2), device=self.device, dtype=self.dtype)
-            v[key].requires_grad = True
-
-        return v
+        return {'mu': VarParam((1, self.K)),
+                'sig': VarParam((1, self.K), init_m=0.0, init_log_s=-2),
+                'w': VarParam((1, self.K - 1), init_m=0.0, init_log_s=-2)}
 
     def subsample_data(self, data, minibatch_info=None):
         if minibatch_info is None:
@@ -42,18 +38,17 @@ class Gmm(advi.Model):
             mini_data = {'y': data['y'][idx]}
         return mini_data
 
-    def sample_real_params(self, v):
-        eta = [torch.randn(1, self.K).double() for vj in v]
-        return {'mu': eta[0] * torch.exp(v['mu'][:, 1]) + v['mu'][:, 0],
-                'sig': eta[1] * torch.exp(v['sig'][:, 1]) + v['sig'][:, 0],
-                'w': eta[2][:, 1:] * torch.exp(v['w'][:, 1]) + v['w'][:, 0]}
+    def sample_real_params(self, vp):
+        real_params = {}
+        for key in vp:
+            real_params[key] = vp[key].sample()
+        return real_params
 
-    def log_q(self, real_params, v):
-        def engine(vj, rj):
-            m, s = vj[:, 0], torch.exp(vj[:, 1])
-            return torch.distributions.Normal(m, s).log_prob(rj).sum()
-
-        return sum([engine(v[key], real_params[key]) for key in v])
+    def log_q(self, real_params, vp):
+        out = 0.0
+        for key in vp:
+            out += vp[key].log_prob(real_params[key]).sum()
+        return out / self.N
 
     def log_prior(self, real_params):
         if self.priors is None:
@@ -72,7 +67,7 @@ class Gmm(advi.Model):
             lpm = torch.distributions.Normal(self.priors['mu'][0],
                     self.priors['mu'][1]).log_prob(real_params['mu']).sum()
 
-            return lpw + lps + lpm
+            return (lpw + lps + lpm) / self.N
 
     def loglike(self, real_params, data, minibatch_info=None):
         sig = torch.exp(real_params['sig'])
@@ -82,12 +77,7 @@ class Gmm(advi.Model):
         # Broadcasting: https://pytorch.org/docs/stable/notes/broadcasting.html
         # mu: 1 x K |  sig: 1 x K | w: 1 x K | y: N x 1
         lpdf = torch.distributions.Normal(mu, sig).log_prob(data['y'])
-        ll = torch.logsumexp(logw + lpdf, 1).sum()
-
-        if minibatch_info is not None:
-            ll *= minibatch_info['N'] / minibatch_info['n']
-
-        return ll
+        return torch.logsumexp(logw + lpdf, 1).mean()
 
     def to_real_space(self, params):
         r = dict()
@@ -102,6 +92,9 @@ class Gmm(advi.Model):
         p['sig'] = torch.exp(real_params['sig'])
         p['w'] = self.sbt(real_params['w'])
         return p
+
+    def vp_as_list(self, vp):
+        return [v.m for v in vp.values()] + [v.log_s for v in vp.values()]
 
     def msg(self, t, v):
         # if (t + 1) % 100 == 0:
@@ -118,38 +111,29 @@ if __name__ == '__main__':
     torch.manual_seed(1)
     np.random.seed(1)
 
-    N = 1000 # works
-    # N = 200 # works, if priors are set (for regularizing)
-
-    # NOTE: When N is small, priors are required so that the variational
-    # parameters do not go to infinity. Priors are (numerically) powerful in
-    # small data settings.
+    # N = 20000 # works
+    N = 10000 # works
+    # N = 200 # works
 
     mu = np.array([3., 1., 2.])
     sig = np.array([.1, .05, .15])
     w = np.array([.5, .3, .2])
     w /= w.sum()
     print("generating data")
-    y = []
-    for i in range(N):
-        k = np.random.choice(3, p=w)
-        y.append(np.random.randn() * sig[k] + mu[k])
+    k = np.random.choice(3, p=w, size=N)
+    y = np.random.randn(N) * sig[k] + mu[k]
 
     y = torch.tensor(y, dtype=torch.float64).reshape(N, 1)
     data = {'y': y}
     K = 3
 
-    # These are good priors. Remember in Gmm.
-    # you must provide some prior information
-    # on how many groups you believe there are.
-    # Otherwise, you might get one big group.
-    priors={'w': torch.zeros(K).double() + N*.01/K,
+    priors={'w': torch.ones(K).double() / K,
             'sig': torch.tensor([1, 10]).double(),
             'mu': torch.tensor([1.85, 5]).double()}
-    mod = Gmm(K=K, priors=priors)
+    mod = Gmm(K=K, N=N, priors=priors)
     out = mod.fit(data, lr=1e-1,
                   minibatch_info={'N': N, 'n': 500},
-                  niters=10000, nmc=1, seed=1, eps=1e-6, init=None,
+                  niters=1000, nmc=1, seed=1, eps=1e-6, init=None,
                   print_freq=100, verbose=1)
 
     # ELBO
